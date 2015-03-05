a36547e5c24bde3cd5fab5bc061d7d8fa1c6ce85
hkasemir <heidi.kasemir@gmail.com>
2015-03-04 22:41:38 -0800

    readability edits

--- tictactoe.py
+++ tictactoe.py
@@ -10,7 +10,7 @@ EMPTY_KEY = ' '
 def create_board(size):
     # Creates 1-dimensional game board for storing played values. 0 is empty,
     # -1 is the computer played value, 1 is the player value.
-    return [0]*size**2
+    return [0] * size ** 2
 
 
 def board_to_string(board):
@@ -18,20 +18,26 @@ def board_to_string(board):
     # space. Adds column numbers and row letters for easy input of cell
     row_length = int(math.sqrt(len(board)))
     board_str = ''
-    for k in range(row_length+1):
+
+    for k in range(row_length + 1):
         board_str += ' ' + str(k) + ' '
     board_str += '\n'
 
     for i in range(row_length):
         row_string = ' ' + chr(65 + i) + ' '
+
         for j in range(row_length):
             key = EMPTY_KEY
-            if board[i*row_length + j] == 1:
+
+            if board[i * row_length + j] == 1:
                 key = PLAYER_KEY
-            elif board[i*row_length + j] == -1:
+            elif board[i * row_length + j] == -1:
                 key = COMPUTER_KEY
+
             row_string += '[' + key + ']'
+
         board_str += row_string + '\n'
+
     return board_str
 
 
@@ -55,7 +61,7 @@ def get_row(board):
     while True:
         row_str = input('Choose a row (A, B, etc): ').upper()
 
-        if ord(row_str) - 65 in range(row_length):
+        if len(row_str) == 1 and ord(row_str) - 65 in range(row_length):
             return ord(row_str) - 65
 
         print('Sorry, please input a letter between A and ' +
@@ -64,13 +70,14 @@ def get_row(board):
 
 def valid_move(row, col, board):
     size = int(math.sqrt(len(board)))
-    index = row*size + col
+    index = row * size + col
     return board[index] == 0
 
 
 def make_move(row, col, board, player):
     size = int(math.sqrt(len(board)))
-    index = row*size + col
+    index = row * size + col
+
     if player == 'Player':
         board[index] = 1
     elif player == 'Computer':
@@ -85,8 +92,9 @@ def get_choice(player, board):
         row = get_row(board)
 
     elif player == 'Computer':
-        column = random.randint(0, size-1)
-        row = random.randint(0, size-1)
+        column = random.randint(0, size - 1)
+        row = random.randint(0, size - 1)
+
     return column, row
 
 
@@ -124,32 +132,39 @@ def check_win(board):
     # returns None if there is a tie.
     for path in WINNING_PATHS:
         check_sum = 0
+
         for i in range(len(path)):
             check_sum += board[path[i]]
+
         if abs(check_sum) == 3:
             return True
     # Check for full board if no wins, indicates a tie
     if 0 not in board:
         return None
+
     return False
 
 
 print('Welcome to Heidi\'s Tic-Tac-Toe game!')
+
 game_board = create_board(BOARD_SIZE)
 print(board_to_string(game_board))
 
 players = ['Player', 'Computer']
 game_complete = False
+
 while game_complete is False:
     for player in players:
         print('Turn: ' + player)
         game_board = game_turn(player, game_board)
         print(board_to_string(game_board))
         turn_result = check_win(game_board)
+
         if turn_result:
             print(player + ' wins!')
             game_complete = True
             break
+
         if turn_result is None:
             print('Catscratch! Nobody wins.')
             game_complete = True
