9b9fd6896c394d2d7068e82124392d58afa3df87
hkasemir <heidi.kasemir@gmail.com>
2015-03-04 20:54:09 -0800

    renamed board_string to board_to_string and check_indices to check_sum for clarity

--- tictactoe.py
+++ tictactoe.py
@@ -6,15 +6,16 @@ PLAYER_KEY = 'x'
 COMPUTER_KEY = 'o'
 EMPTY_KEY = ' '
 
+
 def create_board(size):
     # Creates 1-dimensional game board for storing played values. 0 is empty,
     # -1 is the computer played value, 1 is the player value.
     return [0]*size**2
 
 
-def board_string(board):
-    # converts the numeric values of the game board to 'x', 'o', and blank space
-    # prints with column numbers and row letters for easy input of cell
+def board_to_string(board):
+    # converts the numeric values of the game board to 'x', 'o', and blank
+    # space prints with column numbers and row letters for easy input of cell
     row_length = int(math.sqrt(len(board)))
     board_str = ''
     for k in range(row_length+1):
@@ -57,9 +58,10 @@ def get_row(player, board):
             row_str = input('Sorry, please input a letter between A and ' +
                             str(chr(64 + row_length)) + ': ').upper()
 
-    
+
 def index_from_row_and_col(row, col, size):
         return row*size + col
+
 
 def game_turn(player, board):
     if player == 'Player':
@@ -82,42 +84,41 @@ def game_turn(player, board):
 
 
 def check_win(board):
-    # To be called after player and computer turns. Returns True if 3 in a row 
-    # is made from turn just played. Returns False if no win is detected, and 
+    # To be called after player and computer turns. Returns True if 3 in a row
+    # is made from turn just played. Returns False if no win is detected, and
     # returns None if there is a tie.
     winning_paths = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
     ]
     for path in winning_paths:
-        check_indices = 0
+        check_sum = 0
         for i in range(len(path)):
-            check_indices += board[path[i]]
-        if check_indices == 3 or check_indices == -3:
+            check_sum += board[path[i]]
+        if check_sum == 3 or check_sum == -3:
             return True
     # Check for full board if no wins, indicates a tie
     if 0 not in board:
         return None
     return False
-        
-        
+
 
 print('Welcome to Heidi\'s Tic-Tac-Toe game!')
 game_board = create_board(BOARD_SIZE)
-print(board_string(game_board))
+print(board_to_string(game_board))
 
 players = ['Player', 'Computer']
 game = True
 while game is True:
     for player in players:
         game_board = game_turn(player, game_board)
-        print(board_string(game_board))
+        print(board_to_string(game_board))
         if check_win(game_board):
             print(player + ' wins!')
             game = False
