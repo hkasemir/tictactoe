53a2da52835ad570b3e4a801d450665c6d749ca5
hkasemir <heidi.kasemir@gmail.com>
2015-03-05 18:48:04 -0800

    updated board_to_string function and starting to implement board values of 1 for human, 0 for empty, and -1 for computer

--- tictactoe.py
+++ tictactoe.py
@@ -2,23 +2,43 @@ import random
 
 BOARD_SIZE = 3
 
+row_alpha = []
+for i in range(BOARD_SIZE):
+    row_alpha += chr(65 + i)
+
 
 def create_board(size):
-    return [[' ']*size for i in range(size)]
+    return [[0]*size for i in range(size)]
 
 
-def board_string(board):
+def board_to_string(board):
+    # converts the numeric values of the game board to 'x', 'o', and blank
+    # space. Adds column numbers and row letters for easy input of cell
+    row_length = len(board)
     board_str = ''
-    for k in range(len(board)+1):
+
+    # writes out the column numbers
+    for k in range(row_length + 1):
         board_str += ' ' + str(k) + ' '
     board_str += '\n'
-    for i in range(len(board)):
-        row_string = ' ' + chr(65 + i) + ' '
-        for j in range(len(board)):
-            row_string += '[' + str(board[i][j]) + ']'
-        board_str += row_string + '\n'
-    return board_str
 
+    # puts the A, B, C etc values at the front of the rows
+    for i in range(row_length):
+        row_string = ' ' + row_alpha[i] + ' '
+
+        for j in range(row_length):
+            key = EMPTY_KEY
+
+            if board[i * row_length + j] == 1:
+                key = PLAYER_KEY
+            elif board[i * row_length + j] == -1:
+                key = COMPUTER_KEY
+
+            row_string += '[' + key + ']'
+
+        board_str += row_string + '\n'
+
+    return board_str
 
 def get_column(player, board):
     column_str = input(player + ' choose a column (1, 2, etc): ')
@@ -35,19 +55,18 @@ def get_column(player, board):
 def get_row(player, board):
     row_str = input(player + ' choose a row (A, B, etc): ').upper()
     while True:
-        if ord(row_str) - 65 in range(len(board)):
+        if row_str in row_alpha:
             row_int = ord(row_str) - 65
             return row_int
-        else:
-            row_str = input('Sorry, please input a letter between A and ' +
-                            str(chr(65 + len(board)-1)))
+        row_str = input('Sorry, please input a letter between A and ' +
+                        str(row_alpha[len(board)-1]) + ': ')
 
 
 def player_turn(player, key, board):
     while True:
         column = get_column(player, board)
         row = get_row(player, board)
-        if board[row][column] == " ":
+        if board[row][column] == 0:
             board[row][column] = key
             return board
         else:
@@ -58,9 +77,12 @@ def computer_turn(key, board):
     while True:
         row = random.randint(0, (BOARD_SIZE-1))
         column = random.randint(0, (BOARD_SIZE-1))
-        if board[row][column] == ' ':
+        if board[row][column] == 0:
             board[row][column] = key
             return board
+
+
+IN_A_ROW_TO_WIN = 3
 
 
 def check_win(board):
@@ -68,7 +90,7 @@ def check_win(board):
     # is made from turn just played. Returns False if no win is detected, and 
     # returns None if there is a tie.
     # check for 3 in a row
-    for i in range(len(board)):
+    for i in range(len(board)): # row
         key = board[i][0]
         if key != " " and\
            board[i][1] == key and\
