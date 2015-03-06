03ce5fd21c301dc5949d08cab68308614f1aca7f
hkasemir <heidi.kasemir@gmail.com>
2015-03-05 21:58:41 -0800

    converted board arrays to 1, -1 and 0 for easier check win iteration

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,9 @@
 import random
 
 BOARD_SIZE = 3
+EMPTY_KEY = ' '
+PLAYER_KEY = 'x'
+COMPUTER_KEY = 'o'
 
 row_alpha = []
 for i in range(BOARD_SIZE):
@@ -14,24 +17,23 @@ def create_board(size):
 def board_to_string(board):
     # converts the numeric values of the game board to 'x', 'o', and blank
     # space. Adds column numbers and row letters for easy input of cell
-    row_length = len(board)
     board_str = ''
 
     # writes out the column numbers
-    for k in range(row_length + 1):
+    for k in range(len(board) + 1):
         board_str += ' ' + str(k) + ' '
     board_str += '\n'
 
     # puts the A, B, C etc values at the front of the rows
-    for i in range(row_length):
+    for i in range(len(board)):
         row_string = ' ' + row_alpha[i] + ' '
 
-        for j in range(row_length):
+        for j in range(len(board)):
             key = EMPTY_KEY
 
-            if board[i * row_length + j] == 1:
+            if board[i][j] == 1:
                 key = PLAYER_KEY
-            elif board[i * row_length + j] == -1:
+            elif board[i][j] == -1:
                 key = COMPUTER_KEY
 
             row_string += '[' + key + ']'
@@ -47,9 +49,8 @@ def get_column(player, board):
            int(column_str) - 1 in range(len(board)):
             column = int(column_str) - 1
             return column
-        else:
-            column_str = input('Sorry, please input a number between 1 and ' +
-                               str(len(board)) + ': ')
+        column_str = input('Sorry, please input a number between 1 and ' +
+                           str(len(board)) + ': ')
 
 
 def get_row(player, board):
@@ -59,26 +60,25 @@ def get_row(player, board):
             row_int = ord(row_str) - 65
             return row_int
         row_str = input('Sorry, please input a letter between A and ' +
-                        str(row_alpha[len(board)-1]) + ': ')
+                        str(row_alpha[len(board)-1]) + ': ').upper()
 
 
-def player_turn(player, key, board):
+def player_turn(player, board):
     while True:
         column = get_column(player, board)
         row = get_row(player, board)
         if board[row][column] == 0:
-            board[row][column] = key
+            board[row][column] = 1
             return board
-        else:
-            print('Sorry, that spot\'s been taken, try again: ')
+        print('Sorry, that spot\'s been taken, try again: ')
         
 
-def computer_turn(key, board):
+def computer_turn(board):
     while True:
         row = random.randint(0, (BOARD_SIZE-1))
         column = random.randint(0, (BOARD_SIZE-1))
         if board[row][column] == 0:
-            board[row][column] = key
+            board[row][column] = -1
             return board
 
 
@@ -92,55 +92,52 @@ def check_win(board):
     # check for 3 in a row
     for i in range(len(board)): # row
         key = board[i][0]
-        if key != " " and\
+        if key != 0 and\
            board[i][1] == key and\
            board[i][2] == key:
             return True
     # check for 3 in a column
     for j in range(len(board)):
         key = board[0][j]
-        if key != " " and\
+        if key != 0 and\
            board[1][j] == key and\
            board[2][j] == key:
             return True
     # check for diagonals
     key = board[0][0]
-    if key != " " and\
+    if key != 0 and\
        board[1][1] == key and\
        board[2][2] == key:
         return True
     key = board[2][0]
-    if key != " " and\
+    if key != 0 and\
        board[1][1] == key and\
        board[0][2] == key:
         return True
     # Check for full board if no wins, indicates a tie
     full_row = 0
     for row in board:
-        if ' ' not in row:
+        if 0 not in row:
             full_row += 1
         if full_row == len(board):
             return None
     return False
 
-player_key = 'x' 
-computer_key = 'o'
-
 print('Welcome to Heidi\'s Tic-Tac-Toe game!')
 game_board = create_board(BOARD_SIZE)
-print(board_string(game_board))
+print(board_to_string(game_board))
 
 while True:
-    game_board = player_turn('Player 1', player_key, game_board)
-    print(board_string(game_board))
+    game_board = player_turn('Player 1', game_board)
+    print(board_to_string(game_board))
     if check_win(game_board):
         print('Player wins!')
         break
     if check_win(game_board) is None:
         print('Catscratch! Nobody wins.')
         break
-    game_board = computer_turn(computer_key, game_board)
-    print(board_string(game_board))
+    game_board = computer_turn(game_board)
+    print(board_to_string(game_board))
     if check_win(game_board):
         print('Computer wins!')
         break
